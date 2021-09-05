ef0fa19c5bff36b893a61825dd7d5ce818382bf7
Eugenepro <onetwopro@yandex.ru>
2021-09-05 12:36:18 +0300

    Initial commit

---
+++ backend.py
@@ -0,0 +1,75 @@
+import sqlite3
+
+def connect():
+    conn = sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    conn.commit()
+    conn.close()
+
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL, ?,?,?,?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="", author="", year="", isbn=""):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+# def update(id, title, author, year, isbn):
+#     conn = sqlite3.connect("books.db")
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM book WHERE id=?", (id,))
+#     row = cur.fetchall()[0]
+
+#     args = (title, author, year, isbn)
+#     new_row = []
+#     for arg_enum in enumerate(args, 1):
+#         new_row.append(row[arg_enum[0]] if arg_enum[1] == "" else arg_enum[1])
+#     delete(row[0])
+#     insert(*new_row)
+#     conn.commit()
+#     conn.close()
+
+def update(id, title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+
+    # args = (title, author, year, isbn)
+    # new_row = []
+    # # for arg_enum in enumerate(args, 1):
+    # #     new_row.append(row[arg_enum[0]] if arg_enum[1] == "" else arg_enum[1])
+    # # delete(row[0])
+    # # insert(*new_row)
+
+    cur.execute("UPDATE book * SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id))
+    conn.commit()
+    conn.close()
+
+
+connect()
+# insert("The Earth", "Sam Travalta", 2002, 2345345633)
+
+update(2, "Harry Potter and the goblet of fire", "J.K. Rowling", "", "")
+print(view())
---
+++ frontend.py
@@ -0,0 +1,70 @@
+"""
+A program that stores this book information:
+Title, Author
+Year, ISBN
+
+User can:
+View all records
+Search an entry
+Add entry
+Update entry
+Delete entry
+Close
+"""
+
+from tkinter import *
+import sqlite3
+
+import backend
+
+
+def view_command():
+    for row in backend.view():
+        listbox.insert()
+
+
+window = Tk()
+window.title("Book store")
+title_label = Label(window, text="Title")
+author_label = Label(window, text="Author")
+year_label = Label(window, text="Year")
+isbn_label = Label(window, text="ISBN")
+
+title_text, author_text, year_text, isbn_text = StringVar(), StringVar(), StringVar(), StringVar()
+title_entry = Entry(window, textvariable=title_text)
+author_entry = Entry(window, textvariable=author_text)
+year_entry = Entry(window, textvariable=year_text)
+isbn_entry = Entry(window, textvariable=isbn_text)
+
+title_label.grid(row=0, column=0)
+title_entry.grid(row=0, column=1)
+author_label.grid(row=0, column=2)
+author_entry.grid(row=0, column=3)
+year_label.grid(row=1, column=0)
+year_entry.grid(row=1, column=1)
+isbn_label.grid(row=1, column=2)
+isbn_entry.grid(row=1, column=3)
+
+listbox = Listbox(window, height=6, width=35)
+listbox.grid(row=2, column=0, rowspan=6, columnspan=2)
+scroll = Scrollbar(window)
+scroll.grid(row=2, column=2, rowspan=6)
+
+listbox.configure(yscrollcommand=scroll.set)
+scroll.configure(command=listbox.yview)
+
+view_btn = Button(window, text="View all", width=14, command=view_command)
+search_btn = Button(window, text="Search entry", width=14)
+add_btn = Button(window, text="Add entry", width=14)
+update_btn = Button(window, text="Update", width=14)
+delete_btn = Button(window, text="Delete", width=14)
+close_btn = Button(window, text="Close", width=14)
+
+view_btn.grid(row=2, column=3)
+search_btn.grid(row=3, column=3)
+add_btn.grid(row=4, column=3)
+update_btn.grid(row=5, column=3)
+delete_btn.grid(row=6, column=3)
+close_btn.grid(row=7, column=3)
+
+window.mainloop()
